6da674ead32f3d533cfe543b7e53a2f46f153ba9
Anna An <arzumanyan.an@gmail.com>
2022-05-08 09:37:21 +0400

    Adding the Nav section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
-import { StyledHeader } from "./styles/Header.styled";
+import { StyledHeader, Nav, Logo } from "./styles/Header.styled";
+import { Container } from "./styles/Container.styled";
+import { Button } from "./styles/Button.styled";
 
 export default function Header() {
   return (
     <StyledHeader bg="green">
-      <h1>Hubble</h1>
+      <Container>
+        <Nav>
+          <Logo src="./images/logo.svg" alt="" />
+          <Button>Try It Free</Button>
+        </Nav>
+        <h1>Hubble</h1>
+      </Container>
     </StyledHeader>
   );
 }
