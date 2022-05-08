84dc7798df8f327de362e7bca167f8c2cd286f88
Anna An <arzumanyan.an@gmail.com>
2022-05-08 08:25:13 +0400

    Adding a theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
+import { Container } from "./components/styles/Container.styled";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+};
+
 function App() {
   return (
-    <div className="container">
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>Hello</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
